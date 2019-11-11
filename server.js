69db77f06240cf4d6824312384f753571e35397f
tjjfvi <tjjfvi@tjjfvi.com>
2019-11-11 09:21:01 -0500

    up to rhobo@0.0.19

--- server.js
+++ server.js
@@ -32,20 +32,21 @@ const b = browserify(__dirname + "/static/js/index.js", {
   ],
   cache: {},
   packageCache: {},
-  transform: [ babelify.configure({
-    presets: [
-      "@babel/preset-env",
-      "@babel/preset-react",
-      "@babel/preset-flow",
-    ],
-    plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-      "@babel/plugin-proposal-optional-chaining",
-    ],
-  }) ],
   debug: !!DEBUG,
   plugin: [ watchify ],
+}).transform(babelify, {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-flow",
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
+  ],
+  global: true,
+  ignore: [/\/node_modules\/(?!rhobo\/)/],
 });
 
 b.on("update", bundle);
