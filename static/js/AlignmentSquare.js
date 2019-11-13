65cd82576526f56b5da21e04e1af5f716f0e32fa
tjjfvi <tjjfvi@tjjfvi.com>
2019-11-13 10:09:41 -0500

    fixed bug in AlignmentSquare

--- static/js/AlignmentSquare.js
+++ static/js/AlignmentSquare.js
@@ -3,10 +3,10 @@
 import React from "react";
 
 const AlignmentSquare = ({ cards }: { cards: Array<any>}) => (
-  <div className="AlignmentSquare">
+  <div className="AlignmentSquare" x={console.log(cards)}>
     {["good", "sage", "evil", "wild"].map(a =>
       <div key={a} className={a} style={(n => ({ width: n, height: n }))(
-        Math.sqrt(cards.filter(c => c.factionCode === a).length / cards.length) * 50 + "%"
+        Math.sqrt(cards.filter(c => c.faction.toLowerCase() === a).length / cards.length) * 50 + "%"
       )}/>
     )}
   </div>
