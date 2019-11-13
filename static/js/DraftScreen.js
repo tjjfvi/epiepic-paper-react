23217838229c204dba3fc6fb7deb931568267138
tjjfvi <tjjfvi@tjjfvi.com>
2019-11-13 09:01:34 -0500

    added basic passed/burnt/deck

--- static/js/DraftScreen.js
+++ static/js/DraftScreen.js
@@ -9,6 +9,7 @@ import GameScreen from "./GameScreen";
 
 const DraftCard = ({ id, onClick, selected = false }: { id: number, onClick: () => void, selected?: boolean }) => (
   <div className={"DraftCard " + (selected ? "selected" : "")} onClick={onClick}>
+    <img className="_" src="/1800x334.jpg"/>
     <img src={`/images/${id}`}/>
   </div>
 );
@@ -16,8 +17,11 @@ const DraftScreen = observer<{}>(() => {
   const hand = useValue(() => ws.observable<Array<any>>([], ["hand"]));
   const phase = useValue(() => ws.observable(0, ["phase"]));
   const deck = useObservable<Array<any>>([]);
+  const passed = useObservable<Array<any>>([]);
+  const burnt = useObservable<Array<any>>([]);
   const selected = useObservable<Array<any>>([]);
   const enabled = useComputed(() => selected().length === (phase() % 2 + 1));
+  const active = useObservable<number>(2);
   if(status() === "playing")
     go(GameScreen);
   return <div className="Draft">
@@ -36,16 +40,27 @@ const DraftScreen = observer<{}>(() => {
         selected.to();
       }} selected={selected().includes(c)}/>)}
     </div>
-    <div className="deck">
-      <div>
-        {deck().map(c => <DraftCard key={c._id} id={c._id} onClick={() => previewCard(c)}/>)}
-      </div>
+    <div className="side">
+      {[passed, burnt, deck].map((cs, i) =>
+        <div
+          key={i}
+          data-name={["Passed", "Burnt", "Deck"][i]}
+          className={active() === i ? "active" : ""}
+          onClick={() => active(i)}
+        >
+          <div>
+            {cs().map(c => <DraftCard key={c._id} id={c._id} onClick={() => previewCard(c)}/>)}
+          </div>
+        </div>
+      )}
     </div>
     <button className={"nav " + (enabled() ? "enabled" : "")} onClick={() => {
       if(!enabled())
         return;
       ws.s("select", selected());
+      let rest = hand().filter(c => !selected().includes(c));
       deck().push(...selected());
+      (phase() % 2 ? burnt : passed)().push(...rest);
       selected([]);
       hand([]);
       deck.to();
